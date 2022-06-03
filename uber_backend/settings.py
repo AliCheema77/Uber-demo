7e5b7875f39dbbed03d3cb0cbd8e834535be79c0
AliCheema77 <asadullahcheema77@gmail.com>
2022-06-03 08:55:25 +0500

    FIX: Fixed env issues

--- uber_backend/settings.py
+++ uber_backend/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-gzw(2o3&tc(2a9d-vh*+qa8_94wq%w9$ca$ai5lw&)yg&(7u@k'
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 

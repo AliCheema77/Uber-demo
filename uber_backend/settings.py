e717e592136a86cf33e7954c81ca813284df18d6
AliCheema77 <asadullahcheema77@gmail.com>
2022-06-03 12:11:48 +0500

    Fixed Issues

--- uber_backend/settings.py
+++ uber_backend/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 CLOUDINARY_STORAGE = {
-             'CLOUD_NAME': os.environ.get("CLOUD_NAME"),
+             'CLOUD_NAME': 'dlzrvw7pe',
              'API_KEY': '217375384818832',
              'API_SECRET': 'LWylYpbXLFAl5l2xbQ6LMc3ZAfA'
             }

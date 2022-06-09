5f759a56bcb0a757bd8289c7436044c80ce7d7aa
AliCheema77 <asadullahcheema77@gmail.com>
2022-06-09 16:49:30 +0500

    Fixed issues in pick_drop api

--- services/api/v1/viewsets.py
+++ services/api/v1/viewsets.py
@@ -22,13 +22,13 @@ class PickDropView(APIView):
             local_drivers = []
             pick = lower(serializer.validated_data["pick"])
             destination = lower(serializer.validated_data["destination"])
-            city_pick = pick.split(",")[1]
-            city_destination = pick.split(",")[1]
-            if city_pick == city_destination:
-                return Response({'drivers': []}, status=status.HTTP_200_OK)
+            # city_pick = pick.split(",")[1]
+            # city_destination = pick.split(",")[1]
+            # if city_pick != city_destination:
+            #     return Response({'drivers': []}, status=status.HTTP_200_OK)
             drivers = User.objects.filter(account_type="drive_and_deliver")
             for driver in drivers:
-                if lower(driver.city) in pick or lower(driver.city) in destination:
+                if lower(driver.city) in pick:
                     drivers_data = UserProfileSerializer(driver, many=False)
                     local_drivers.append(drivers_data.data)
             return Response({'drivers': local_drivers}, status=status.HTTP_200_OK)

5335291ae4414f6919e3d7a760eded3981c8a7ab
AliCheema77 <asadullahcheema77@gmail.com>
2022-06-17 16:54:04 +0500

    Enhance the functionality of RiderRequestSerializer

--- services/api/v1/serializers.py
+++ services/api/v1/serializers.py
@@ -9,27 +9,33 @@ class PickDropSerializer(serializers.Serializer):
 
 
 class RiderRequestSerializer(serializers.ModelSerializer):
+    requester = UserProfileSerializer(many=False, read_only=True)
+    deriver = UserProfileSerializer(many=False, read_only=True)
 
     class Meta:
         model = RiderRequest
         fields = ['id', 'destination_label', 'pickup_label', 'destination_coordinates', 'pickup_coordinates',
                   'status', 'requester', 'deriver']
 
-    def to_representation(self, instance):
-        data = super().to_representation(instance)
-        data["requester"] = {
-            "id": instance.requester.id,
-            "username": instance.requester.username,
-            "email": instance.requester.email,
-            "phone_number": instance.requester.phone_number
-            }
-        data["deriver"] = {
-            "id": instance.deriver.id,
-            "username": instance.deriver.username,
-            "email": instance.deriver.email,
-            "phone_number": instance.deriver.phone_number
-            }
-        return data
+    # def to_representation(self, instance):
+    #     data = super().to_representation(instance)
+    #     data["requester"] = {
+    #         "id": instance.requester.id,
+    #         "first_name": instance.requester.first_name,
+    #         "last_name": instance.requester.last_name,
+    #         "username": instance.requester.username,
+    #         "email": instance.requester.email,
+    #         "phone_number": instance.requester.phone_number
+    #         }
+    #     data["deriver"] = {
+    #         "id": instance.deriver.id,
+    #         "first_name": instance.deriver.first_name,
+    #         "last_name": instance.deriver.last_name,
+    #         "username": instance.deriver.username,
+    #         "email": instance.deriver.email,
+    #         "phone_number": instance.deriver.phone_number
+    #         }
+    #     return data
 
 
 class DriverAcceptCancelRideSerializer(serializers.Serializer):
